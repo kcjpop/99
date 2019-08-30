3231f5805c164e3a36f94f86ce6a04965674c965
An <kcjpop@users.noreply.github.com>
2019-08-30 12:57:05 +0300

    Create .drone.yml
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://cloud.drone.io/api/badges/kcjpop/99/status.svg)](https://cloud.drone.io/kcjpop/99)
+
+99 problems (and hopefully more) in various languages
---
+++ clj/test/clj/count_digit_test.clj
@@ -0,0 +1,13 @@
+(ns clj.count-digit-test
+  (:require [clojure.test :refer :all]
+            [clj.count-digit :refer :all]))
+
+(deftest test-count-digit
+  (testing "count digits"
+    (is (= (nb-dig 10 1) 4))
+    (is (= (nb-dig 25 1) 11))
+    (is (= (nb-dig 5750 0) 4700))
+    (is (= (nb-dig 11011 2) 9481))
+    (is (= (nb-dig 12224 8) 7733))
+    (is (= (nb-dig 11549 1) 11905))
+    ))
